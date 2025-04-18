51f2e4588d7a26dd47f83e3d09c974066630152d
AkpolatBerkin <berkinakpolat@gmail.com>
2025-04-18 19:38:41 +0200

    init

---
+++ src/test/java/de/htwberlin/webtech/rezeptapp/RezeptappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.rezeptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezeptappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
